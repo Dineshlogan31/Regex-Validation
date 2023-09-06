4017a87064d7b4df528e327c9877184c072e5b6d
DineshLogan <dineshlogan31@gmail.com>
2023-09-06 12:26:13 +0530

    Final

---
+++ Regex-Form-Validation/index.js
@@ -0,0 +1,24 @@
+const inputs = document.querySelectorAll("input");
+
+const patterns = {
+  telephone: /^\d{10}$/,
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\w@-]{8,20}$/,
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+};
+
+//validation method
+function validation(field, regex) {
+  if (regex.test(field.value)) {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    validation(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
